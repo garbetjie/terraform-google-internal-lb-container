c53130afae87ab116d8eb190de99390465dcceff
Geoff Garbers <geoff@garbers.co.za>
2020-10-29 09:42:21 +0200

    Update health check ip ranges for autohealing

--- firewall.tf
+++ firewall.tf
@@ -1,12 +1,27 @@
-resource google_compute_firewall allowed_routes {
-  for_each = var.regions
-  name = "fluentd-${each.value}"
+//resource google_compute_firewall allowed_routes {
+//  for_each = var.regions
+//  name = "fluentd-${each.value}"
+//  network = "default"
+//  source_ranges = concat(data.google_compute_subnetwork.subnetworks[each.value].ip_cidr_range, var.additional_allowed_source_ranges)
+//  target_tags = ["fluentd-${each.value}"]
+//
+//  allow {
+//    protocol = "TCP"
+//    ports = local.ports
+//  }
+//}
+
+resource google_compute_firewall health_checks {
+  name = local.prefix
+  source_ranges = ["130.211.0.0/22", "35.191.0.0/16"]
   network = "default"
-  source_ranges = concat(["10.128.0.0/9"], var.additional_allowed_source_ranges)
-  target_tags = ["fluentd-${each.value}"]
+  target_tags = ["${local.prefix}-fw"]
 
   allow {
     protocol = "TCP"
-    ports = local.ports
+  }
+
+  allow {
+    protocol = "UDP"
   }
 }
