68d9000c1f348518187eb79bb03aebea619992bb
Geoff Garbers <geoff@garbers.co.za>
2020-10-29 13:09:16 +0200

    Better handle TCP ports.

--- firewall.tf
+++ firewall.tf
@@ -1,16 +1,3 @@
-//resource google_compute_firewall allowed_routes {
-//  for_each = var.regions
-//  name = "fluentd-${each.value}"
-//  network = "default"
-//  source_ranges = concat(data.google_compute_subnetwork.subnetworks[each.value].ip_cidr_range, var.additional_allowed_source_ranges)
-//  target_tags = ["fluentd-${each.value}"]
-//
-//  allow {
-//    protocol = "TCP"
-//    ports = local.ports
-//  }
-//}
-
 resource google_compute_firewall health_checks {
   name = local.prefix
   source_ranges = ["130.211.0.0/22", "35.191.0.0/16"]
@@ -19,9 +6,15 @@ resource google_compute_firewall health_checks {
 
   allow {
     protocol = "TCP"
+    ports = concat([22], local.tcp_ports)
   }
 
-  allow {
-    protocol = "UDP"
+  dynamic "allow" {
+    for_each = length(local.udp_ports) > 0 ? [local.udp_ports] : []
+
+    content {
+      protocol = "UDP"
+      ports = allow.value
+    }
   }
 }
