33ac103f8ef0d656d928a0fc122f9be28af69001
Geoff Garbers <geoff@garbers.co.za>
2020-10-29 13:10:02 +0200

    Ensure single port is selected for health check.

--- health_check.tf
+++ health_check.tf
@@ -3,32 +3,20 @@ resource google_compute_health_check load_balancer {
   check_interval_sec = 3
   timeout_sec = 2
   unhealthy_threshold = 2
+  healthy_threshold = 1
 
   tcp_health_check {
-    port = 22
-  }
-
-  dynamic "tcp_health_check" {
-    for_each = local.tcp_ports
-    content {
-      port = tcp_health_check.value
-    }
+    port = length(local.tcp_ports) > 0 ? local.tcp_ports[0] : 22
   }
 }
 
 resource google_compute_health_check instance_group {
   name = "${local.prefix}-ig"
-  check_interval_sec = 10
+  check_interval_sec = 30
   unhealthy_threshold = 3
+  healthy_threshold = 1
 
   tcp_health_check {
-    port = 22
-  }
-
-  dynamic "tcp_health_check" {
-    for_each = local.tcp_ports
-    content {
-      port = tcp_health_check.value
-    }
+    port = length(local.tcp_ports) > 0 ? local.tcp_ports[0] : 22
   }
 }
